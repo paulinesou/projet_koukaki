cdd1e29a0a3770f6986928a5047d9a6c4c071569
PaulineSou <pauline.soudry@gmail.com>
2023-05-15 11:28:24 +0200

    ajustement menu burger

--- foce-child/templates/menu_burger.php
+++ foce-child/templates/menu_burger.php
@@ -1,10 +1,12 @@
 <div id="menu_burger" class="nav_burger">
   <a id="closeBtn" href="#" class="close">&times;</a>
   <ul>
+    <img class="logo_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo_burger.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
     <li><a href="#story">Histoire</a></li>
     <li><a href="#characters">Personnages</a></li>
     <li><a href="#place">Lieu</a></li>
     <li><a href="#studio">Studio Koukaki</a></li>
+    <img class="logo_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/studio_koukaki_burger.png'; ?> " alt="logo Studio Koukaki">
   </ul>
 </div>
 
